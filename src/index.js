f9c9d1d3746e62f3584d1d347f229b7676f5febe
amjad awad <amjadawad129@gmail.com>
2025-07-11 15:42:57 +0300

    update the return

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ mongoose
 
 app.get("/", (req, res) => {
     redisClient.set("products", "products...");
-    res.send("<h1>Welcome to My Express Server hi hhff</h1>");
+    res.send("<h1>Welcome to My Express Server in AWS</h1>");
 })
 
 app.get("/data", async (req, res) => {
